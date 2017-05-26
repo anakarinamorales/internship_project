d0f7d1a22813c9eb5feec5cffe09f9e1b8a24d1c
Nkarinamorales <akarinammachado@gmail.com>
2017-05-26 09:18:52 -0300

    Estruturação inicial de pastas

--- src/models/AddressInterface.php
+++ src/models/AddressInterface.php
@@ -1,11 +1,6 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: 13bits
- * Date: 24/05/2017
- * Time: 11:32
- */
+
 class AddressInterface
 {
 
--- src/models/ServiceTypeInterface.php
+++ src/models/ServiceTypeInterface.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: 13bits
- * Date: 24/05/2017
- * Time: 11:42
- */
 class ServiceTypeInterface
 {
 
