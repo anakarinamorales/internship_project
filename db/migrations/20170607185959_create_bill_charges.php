451b97beb0d6417842f47fd01de7a89f9851d086
Ana Karina <akarinammachado@gmail.com>
2017-06-07 16:04:26 -0300

    Migrando tabela bill_charges

--- db/migrations/20170607185959_create_bill_charges.php
+++ db/migrations/20170607185959_create_bill_charges.php
@@ -4,29 +4,18 @@ use Phinx\Migration\AbstractMigration;
 
 class CreateBillCharges extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
+    public function up()
     {
+        $this->table('bill_charges')
+            ->addColumn('billing_date', 'datetime')
+            ->addColumn('value', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->save();
+    }
 
+    public function down()
+    {
+        $this->dropTable('bill_charges');
     }
 }
